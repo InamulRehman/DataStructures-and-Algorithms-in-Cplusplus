c462e54d3bc651a8ed78a9295c4591807271e6f4
InamulRehman <inamulrehman09@gmail.com>
2021-09-01 14:12:51 +0500

    Program to sort an array using bubble sort added

---
+++ Searching&Sorting/Sorting/BubbleSort.cpp
@@ -0,0 +1,42 @@
+// Program to sort an array of ten elements using in ascending order 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int num[10],temp;
+    cout << "Enter values in an array: " << flush;
+    for (int i=0; i <=9; i++)
+    {
+        cout << "\n" << i << "`````````````` " ;
+        cin >> num[i];
+    }
+    ////////////////////////////////////////////////////////
+    cout << "Values in array without sorting: " ;
+    for (int i=0; i <=9; i++)
+    {
+        cout << "\n" << i << "|||||||||||||||||| " ;
+        cout << num[i];
+    }
+    ////////////////////////////////////////////////////////
+    for (int i=0; i <= 8; i++)
+    {
+        for(int j=0; j<= 9; j++)
+        {
+            if (num[j] > num[j+1])
+            {
+                temp = num[j];
+                num[j] = num[j+1];
+                num[j+1] = temp;
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////
+    cout << "\nValues in array after sorting: " ;
+    for (int i=0; i <=9; i++)
+    {
+        cout << "\n" << i << "~~~~~~~~~~~~~~~ " ;
+        cout << num[i];
+    }
+    
+}
